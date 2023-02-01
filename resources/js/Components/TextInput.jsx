b2cacb2d30094c5a8c088eeda689b5ba34282e5c
asep.cahyudin <asep.cahyudin92@gmail.com>
2023-02-01 22:14:57 +0700

    edit components input

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,7 +1,7 @@
 import { forwardRef, useEffect, useRef } from 'react';
 
 export default forwardRef(function TextInput(
-    { type = 'text', name, id, value, className, autoComplete, required, isFocused, handleChange },
+    { type = 'text', name, id, value, defaultValue, className, variant ="primary" , autoComplete, required, isFocused, handleChange, placeholder, isError},
     ref
 ) {
     const input = ref ? ref : useRef();
@@ -20,13 +20,14 @@ export default forwardRef(function TextInput(
                 id={id}
                 value={value}
                 className={
-                    `border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ` +
+                    `rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none` +
                     className
                 }
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
